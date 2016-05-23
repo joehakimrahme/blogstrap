24f355bc76b1d9794331573eb0b470166e8fbeea
Joe H. Rahme <joehakimrahme@gmail.com>
2016-05-23 19:33:10 +0200

    Bumps to 0.3.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,8 +19,8 @@ project = u'Blogstrap'
 copyright = u'2016, Joe H. Rahme'
 author = u'Joe H. Rahme'
 
-version = '0.3.0'
-release = '0.3.0'
+version = '0.3.1'
+release = '0.3.1'
 
 language = None
 exclude_patterns = []
