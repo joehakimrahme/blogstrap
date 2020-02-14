54843da7dc00dd5ef009b4fb5307172b72dfa845
Joe H. Rahme <joehakimrahme@gmail.com>
2020-02-14 12:45:48 +0100

    Make page load more elegant
    Because of the showdown modifies the rendering of the page, there's a
    split second on load where the unstyled markdown was shown to the user
    before being blogstrapped.
    
    This commit hides this behavior by turning off the visibility of the
    body until all the rendering is done.
    
    It also avoids messing up the order of js loading before this.

--- blogstrap/static/blogstrap.js
+++ blogstrap/static/blogstrap.js
@@ -1,10 +1,13 @@
 var converter = new showdown.Converter();
-var blogpost = document.getElementsByClassName("blogstrap");
 
-for (div of blogpost) {
-  text = div.textContent.trim().split('/\n */').join('\n');
-  var html = converter.makeHtml(text);
-  div.innerHTML = html;
-}
+window.onload = function () {
+  var blogpost = document.getElementsByClassName("blogstrap");
+  //TODO: ES6 only. Provide support for older browsers
+  for (div of blogpost) {
+    text = div.textContent.trim().split('/\n */').join('\n');
+    var html = converter.makeHtml(text);
+    div.innerHTML = html;
+  }
 
-document.body.style.visibility = "visible";
+  document.body.style.visibility = "visible";
+};
