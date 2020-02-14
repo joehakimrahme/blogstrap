bfb2a0b4d74cbf7be860477a87b83b57b3b921a5
Joe H. Rahme <joehakimrahme@gmail.com>
2020-02-14 17:05:29 +0100

    Adds table support in mardown
    By default, Showdown doesn't support tables. To be fair, tables are
    not standard markdown. This patch enables table support in the
    dependency and adds the proper bootstrap classes to the elements.

--- blogstrap/static/blogstrap.js
+++ blogstrap/static/blogstrap.js
@@ -1,12 +1,22 @@
-var converter = new showdown.Converter();
+var converter = new showdown.Converter({
+  tables: true,
+  strikethrough: true
+});
 
 window.onload = function () {
+
   var blogpost = document.getElementsByClassName("blogstrap");
   //TODO: ES6 only. Provide support for older browsers
   for (div of blogpost) {
     text = div.textContent.trim().split('/\n */').join('\n');
     var html = converter.makeHtml(text);
     div.innerHTML = html;
+  }
+
+  // Add .table to our generated tables
+  tables = blogpost[0].getElementsByTagName("table");
+  for (table of tables) {
+    table.classList.add("table");
   }
 
   document.body.style.visibility = "visible";
